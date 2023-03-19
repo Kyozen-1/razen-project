140bc3b3ea742424554b26333a1403811bfef577
Kyozen-1 <skadi1268@gmail.com>
2023-03-20 01:16:09 +0700

    fixed

--- resources/views/razen-project/admin/layouts/footer.blade.php
+++ resources/views/razen-project/admin/layouts/footer.blade.php
@@ -1,13 +1,16 @@
+@php
+    use Carbon\Carbon;
+@endphp
 <!-- Layout Footer Start -->
 <footer>
     <div class="footer-content">
     <div class="container">
         <div class="row">
         <div class="col-12 col-sm-6">
-            <p class="mb-0 text-muted text-medium">Colored Strategies 2021</p>
+            <p class="mb-0 text-muted text-medium">PT Razen Teknologi Indonesia {{Carbon::now()->year}}</p>
         </div>
         <div class="col-sm-6 d-none d-sm-block">
-            <ul class="breadcrumb pt-0 pe-0 mb-0 float-end">
+            {{-- <ul class="breadcrumb pt-0 pe-0 mb-0 float-end">
             <li class="breadcrumb-item mb-0 text-medium">
                 <a href="https://1.envato.market/BX5oGy" target="_blank" class="btn-link">Review</a>
             </li>
@@ -17,7 +20,7 @@
             <li class="breadcrumb-item mb-0 text-medium">
                 <a href="https://acorn-html-docs.coloredstrategies.com/" target="_blank" class="btn-link">Docs</a>
             </li>
-            </ul>
+            </ul> --}}
         </div>
         </div>
     </div>
--- resources/views/razen-project/admin/layouts/head.blade.php
+++ resources/views/razen-project/admin/layouts/head.blade.php
@@ -1,7 +1,12 @@
+@php
+    use App\Models\RazenProject\Admin\ProfilRazenProject;
+
+    $profil = ProfilRazenProject::first();
+@endphp
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 <title>@yield('title', 'Admin | Dashboard')</title>
-<meta name="description" content="Acorn elearning platform dashboard." />
+<meta name="description" content="Admin Panel Razen Project" />
 <!-- Favicon Tags Start -->
 <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/apple-touch-icon-57x57.png') }}" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/apple-touch-icon-114x114.png') }}" />
@@ -11,11 +16,11 @@
 <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/apple-touch-icon-120x120.png') }}" />
 <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/apple-touch-icon-76x76.png') }}" />
 <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/apple-touch-icon-152x152.png') }}" />
-<link rel="icon" type="image/png" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/favicon-196x196.png') }}" sizes="196x196" />
-<link rel="icon" type="image/png" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/favicon-96x96.png') }}" sizes="96x96" />
-<link rel="icon" type="image/png" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/favicon-32x32.png') }}" sizes="32x32" />
-<link rel="icon" type="image/png" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/favicon-16x16.png') }}" sizes="16x16" />
-<link rel="icon" type="image/png" href="{{ asset('acorn/acorn-elearning-portal/img/favicon/favicon-128.png') }}" sizes="128x128" />
+<link rel="icon" type="image/png" href="{{ asset('images/razen-project/logo/'.$profil->logo) }}" sizes="196x196" />
+<link rel="icon" type="image/png" href="{{ asset('images/razen-project/logo/'.$profil->logo) }}" sizes="96x96" />
+<link rel="icon" type="image/png" href="{{ asset('images/razen-project/logo/'.$profil->logo) }}" sizes="32x32" />
+<link rel="icon" type="image/png" href="{{ asset('images/razen-project/logo/'.$profil->logo) }}" sizes="16x16" />
+<link rel="icon" type="image/png" href="{{ asset('images/razen-project/logo/'.$profil->logo) }}" sizes="128x128" />
 <meta name="application-name" content="&nbsp;" />
 <meta name="msapplication-TileColor" content="#FFFFFF" />
 <meta name="msapplication-TileImage" content="{{ asset('acorn/acorn-elearning-portal/img/favicon/mstile-144x144.png') }}" />
